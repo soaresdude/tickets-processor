542231b3bd980d60251c0d05680db5fc645be817
Yuri Araújo <araujoyuri@edu.unifor.br>
2022-10-10 23:40:29 -0300

    refactor: remove hello route

--- tickets_processor/main.py
+++ tickets_processor/main.py
@@ -12,8 +12,3 @@ app = FastAPI()
 
 app.include_router(health.router)
 app.include_router(tickets.router)
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
